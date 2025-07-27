18ad9fd70f344a169185e1ebb3a2f2e838f304f6
Atakan <ata.kaner03@gmail.com>
2025-07-27 16:37:57 +0300

    gts7l: Use Tiramisu build description
    Change-Id: I061e48f98d253935f3f5b12a4a499b99aeec529f

--- lineage_gts7l.mk
+++ lineage_gts7l.mk
@@ -25,7 +25,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 # Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="gts7lxx-user 11 RP1A.200720.012 T875XXS8DXH1 release-keys" \
+    BuildDesc="gts7lxx-user 13 TP1A.220624.014 T875XXS8DXH1 release-keys" \
     BuildFingerprint=samsung/gts7lxx/gts7l:11/RP1A.200720.012/T875XXS8DXH1:user/release-keys \
     DeviceProduct=gts7lxxx \
     SystemName=gts7lxxx
